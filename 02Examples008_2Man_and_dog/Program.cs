512273d13bedc2ef52b9e26587dd2a7735105885
Timofey <kornilinT@gmail.com>
2022-09-08 00:59:36 +0300

    Задача: Два друга и собака

---
+++ 02Examples008_2Man_and_dog/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine(" ");
+Console.WriteLine(" ");
+Console.WriteLine("*** Начало программы ***");
+Console.WriteLine(" ");
+
+int count = 0;
+double distance = 10000;
+int firstFriendSpeed = 3;
+int secondFriendSpeed = 5;
+int dogSpeed = 7;
+int friendDog = 2;
+double time = 0;
+
+while(distance>1)
+{
+ if(friendDog == 1)
+ {
+     time = distance/(secondFriendSpeed + dogSpeed);
+     friendDog = 2;
+ }
+ else
+ {
+     time = distance/(firstFriendSpeed + dogSpeed);
+     friendDog = 1;
+ }
+
+   distance=distance-(firstFriendSpeed+secondFriendSpeed)*time;
+   count = count + 1;
+}
+
+Console.Write("До встречи двух друзей собака пробежала от одного друга к другому ");
+Console.Write(count);
+Console.WriteLine(" раз.");
+Console.WriteLine(" ");
+Console.WriteLine("*** Конец прграммы ***");
+Console.WriteLine(" ");
+Console.WriteLine(" ");
