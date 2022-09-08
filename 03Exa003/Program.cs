cb497c361f37f6b07caae1afaef06df3cd1b29cd
Timofey <kornilinT@gmail.com>
2022-09-08 20:14:22 +0300

    Дни недели2

--- 03Exa003/Program.cs
+++ 03Exa003/Program.cs
@@ -12,47 +12,14 @@ int number = int.Parse(Console.ReadLine());
 Console.WriteLine(" ");
 Console.Write("День недели: ");
 
-if (number == 1)
-{
- Console.WriteLine("Понедельник");
-}
-else
-{
- if (number == 2)
- {
-  Console.WriteLine("Вторник");
- }
- else
- {
-  if (number == 3)
-  {
-   Console.WriteLine("Среда");
-  }
-  else
-  {
-   if (number == 4)
-   {
-    Console.WriteLine("Четверг");
-   }
-   else
-   {
-    if (number == 5)
-    {
-     Console.WriteLine("Пятница");
-    }
-    else
-    {
-     if (number == 6)
-     {
-      Console.WriteLine("Суббота");
-     }
-     else
-     {
-      if (number == 7)
-      {
-       Console.WriteLine("Воскресенье");
-      }
-      else
+if (number == 1) { Console.WriteLine("Понедельник"); }
+else { if (number == 2) { Console.WriteLine("Вторник"); }
+ else { if (number == 3) { Console.WriteLine("Среда"); }
+  else { if (number == 4) { Console.WriteLine("Четверг"); }
+   else { if (number == 5) { Console.WriteLine("Пятница"); }
+    else { if (number == 6) { Console.WriteLine("Суббота"); }
+     else { if (number == 7) { Console.WriteLine("Воскресенье"); }
+      else 
       {
         Console.WriteLine(" ✌ Нет дня недели с таким номером ✌");
       }
