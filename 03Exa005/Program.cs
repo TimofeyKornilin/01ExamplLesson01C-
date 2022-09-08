1ad9ae479baf6719fba05b51edc10fdd384bdc8e
Timofey <kornilinT@gmail.com>
2022-09-08 22:58:10 +0300

    Вывод последней цифры трёхзначного числа, с проверкой вводимого числа

--- 03Exa005/Program.cs
+++ 03Exa005/Program.cs
@@ -4,17 +4,24 @@
 Console.WriteLine(" ");
 Console.WriteLine(" ");
 Console.WriteLine("*** Начало программы ***");
+label1:
 Console.WriteLine(" ");
-
+Console.WriteLine(" ");
 Console.Write("Введите трёхзначное положительное число: ");
 int number = int.Parse(Console.ReadLine());
 
+if (number>100&&number<1000)
+{
 //Console.WriteLine (number % 10);
-
 int div = number/10;
 int numberLast = number - (div*10);
 Console.Write($"Последняя цифра введёного числа: {numberLast}");
-
+}
+else
+{
+ Console.Write("Введено не верное значение, повторите ввод");
+ goto label1;
+}
 
 Console.WriteLine(" ");
 Console.WriteLine(" ");
