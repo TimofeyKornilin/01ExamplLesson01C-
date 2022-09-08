3aded95c9c93a76df3ed8ba9d3306c64d563fcd3
Timofey <kornilinT@gmail.com>
2022-09-08 16:12:18 +0300

    Программа вычисление квадрата и куба введённого числа

---
+++ 03Exa001/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программы вычесление квадрата и куба введённого числа.
+
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+Console.WriteLine("*** Начало программы ***");
+Console.WriteLine(" ");
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sqr = number*number;
+Console.WriteLine($"Квадрат числа {number} равен: {sqr}");
+
+int sqr1 = Convert.ToInt32(Math.Pow(number,3));
+Console.WriteLine($"Куб числа {number} равен: {sqr1}");
+
+Console.WriteLine(" ");
+Console.WriteLine("*** Конец программы ***");
+Console.WriteLine(" ");
+Console.WriteLine(" ");
