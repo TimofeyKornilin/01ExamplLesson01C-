8b5ba18c17780a68ef855b875eaa6050c9e4f2ca
Timofey <kornilinT@gmail.com>
2022-09-09 00:37:23 +0300

    Поиск большего числа из 3х введённых

---
+++ Lesson1HW02/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine(" \n");
+Console.WriteLine("*** Начало программы ***\n");
+
+Console.Write("Введите первое целое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье целое число: ");
+int number3 = int.Parse(Console.ReadLine());
+Console.WriteLine(" ");
+
+int max = number1;
+if (number2>max) max=number2;
+if (number3>max) max=number3;
+ 
+Console.WriteLine($"Максимальное число: {max}");
+
+Console.WriteLine(" ");
+Console.WriteLine("*** Конец программы ***\n");
