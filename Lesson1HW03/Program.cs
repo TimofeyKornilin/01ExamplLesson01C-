bac797c2b577c4fdab601838b6f061b313fde092
Timofey <kornilinT@gmail.com>
2022-09-09 01:54:12 +0300

    Определяет чётное число или нет

---
+++ Lesson1HW03/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine(" \n");
+Console.WriteLine("*** Начало программы ***\n");
+
+Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(" ");
+
+int number2 = ((number*10)/2)%10;
+
+if (number2==0)
+Console.WriteLine("Да, введённое число являеться чётным");
+else
+Console.WriteLine("Нет, введённое число не являеться чётным");
+
+Console.WriteLine(" ");
+Console.WriteLine("*** Конец программы ***\n");
+Console.WriteLine(" ");
